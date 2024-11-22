8bc33f9d09f65bb39ab6760f5d863102d04fd559
hikmah76 <nur.hikmah190504@gmail.com>
2024-11-22 20:10:15 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const todoRoutes = require('./routes/tododb.js')
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
